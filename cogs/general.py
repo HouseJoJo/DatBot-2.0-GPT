7279db95dd29c02e5dea9225114300ffbddcdea3
Josue S <68451593+HouseJoJo@users.noreply.github.com>
2023-02-07 17:34:56 -0500

    Add example command in general cog

--- cogs/general.py
+++ cogs/general.py
@@ -2,5 +2,14 @@ import discord
 from discord.ext import commands
 
 class General(commands.Cog):
+
     def __init__(self, bot):
         self.bot = bot
+
+    @discord.slash_command()
+    async def info(self, ctx):
+        await ctx.send("")
+
+def setup(bot):
+    bot.add_cog(General(bot))
+    
